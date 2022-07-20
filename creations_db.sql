454d335f4b3a719170ed9fcf16ad9c6257ba00b2
Maxim Partin <max.physmath.05@gmail.com>
2022-07-20 11:14:41 +0300

    update sql db creations query

--- creations_db.sql
+++ creations_db.sql
@@ -1,69 +1,73 @@
-CREATE TABLE customers (
-    customer_id varchar(40) NOT NULL PRIMARY KEY,
-    name text NOT NULL,
-    phone varchar(20) NOT NULL,
-    zip varchar(10) NOT NULL,
-    city text NOT NULL,
-    address text NOT NULL,
-    region text NOT NULL,
-    email text NOT NULL
+CREATE TABLE orders (
+    id serial PRIMARY KEY,
+    order_uid varchar(40) NOT NULL,
+    track_number varchar(40) NOT NULL,
+    entry text,
+    locale text,
+    internal_signature text,
+    customer_id text NOT NULL ,
+    delivery_service text,
+    shardkey text,
+    sm_id integer NOT NULL,
+    date_created date DEFAULT NOW(),
+    oof_shard text
 );
 
 CREATE TABLE items (
-    chrt_id varchar(40) NOT NULL PRIMARY KEY,
-    price numeric(6, 2) NOT NULL,
-    rid varchar(30) NOT NULL,
-    name text NOT NULL,
-    sale int CHECK ( sale >= 0 AND sale <= 100 ),
-    size varchar(10),
-    nm_id int,
-    brand text
-);
-
-CREATE TABLE orders (
-    order_uid varchar(40) NOT NULL PRIMARY KEY,
-    customer_id varchar(40) NOT NULL,
-    track_number varchar(14) NOT NULL,
-    entry text NOT NULL,
-    locale varchar(3) NOT NULL,
-    internal_signature text NOT NULL,
-    delivery_service text NOT NULL,
-    shard_key varchar(10) NOT NULL,
-    sm_id int,
-    date_created date NOT NULL DEFAULT NOW(),
-    oof_shard varchar(10),
-    FOREIGN KEY (customer_id)
-    REFERENCES customers(customer_id)
+    id serial PRIMARY KEY,
+    chrt_id integer NOT NULL,
+    track_number varchar(40) NOT NULL,
+    price numeric(6, 2),
+    rid text,
+    name text,
+    sale integer,
+    size integer,
+    total_price numeric(6, 2),
+    nm_id integer NOT NULL,
+    brand text,
+    status integer,
+    FOREIGN KEY (track_number)
+    REFERENCES orders(track_number)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE order_items (
-    order_id varchar(40) NOT NULL,
-    item_id varchar(40) NOT NULL,
-    status INT NOT NULL,
-    PRIMARY KEY (order_id, item_id),
+CREATE TABLE delivery (
+    id serial PRIMARY KEY,
+    order_id integer,
+    name text,
+    phone text,
+    zip text,
+    city text,
+    address text,
+    region text,
+    email text,
     FOREIGN KEY (order_id)
-    REFERENCES orders(order_uid)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    FOREIGN KEY (item_id)
-    REFERENCES items(chrt_id)
+    REFERENCES orders(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 CREATE TABLE payments (
-    transaction varchar(40) NOT NULL PRIMARY KEY,
-    request_id text,
-    currency varchar(5) NOT NULL,
-    provider varchar(10) NOT NULL,
-    payment_dt int NOT NULL,
-    bank text NOT NULL,
-    delivery_cost numeric(6, 2) NOT NULL,
-    goods_total int NOT NULL,
-    custom_fee numeric(6, 2) NOT NULL,
-    FOREIGN KEY (transaction) REFERENCES customers(customer_id)
+    id serial PRIMARY KEY,
+    transaction varchar(40) NOT NULL,
+    request_id text NOT NULL,
+    currency varchar(5),
+    provider text,
+    amount integer,
+    payment_id integer NOT NULL,
+    bank text,
+    delivery_coast numeric(6, 2),
+    goods_total integer,
+    custom_fee integer,
+    FOREIGN KEY (transaction)
+    REFERENCES orders(order_uid)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
+
+CREATE TABLE invalid_messages(
+    id serial PRIMARY KEY,
+    data jsonb
+);
+
